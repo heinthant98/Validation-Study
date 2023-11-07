66d9385f48d3d2246c2cedd34a282595cd861b6b
HeinThantAung <heinthant098@gmail.com>
2023-11-07 11:48:59 +0630

    プロジェクトの作成

---
+++ src/test/java/com/example/validationStudy/ValidationStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validationStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
